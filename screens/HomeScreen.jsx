9e93d23d6388e754c7b75b7972cc0f77f2fae510
anderlfrias <anderl.friaas@gmail.com>
2022-11-03 14:49:06 -0400

    Pantalla de inicio, lista

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,14 +1,35 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import React from 'react'
+import { View, Text, SafeAreaView, Image, StyleSheet } from 'react-native'
 
-export class HomeScreen extends Component {
-    render() {
-        return (
-        <View>
-            <Text>HomeScreen</Text>
-        </View>
-        )
-    }
+const HomeScreen = () => {
+    return (
+        <SafeAreaView>
+            <View style={styles.container}>
+                <Text style={styles.title}>Inicio</Text>
+                <Image style={styles.image} source={require('../assets/home-images/navaja-suiza.jpg')} />
+            </View>
+        </SafeAreaView>
+    )
 }
 
-export default HomeScreen
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+    },
+    image: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        overflow: 'hidden',
+        marginTop: 20,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
+})
