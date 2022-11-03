54d9b8b105dc2cc16ae41c6cbf6e26d1ba89862f
anderlfrias <anderl.friaas@gmail.com>
2022-11-03 11:30:59 -0400

    Se agregaron iconos en el tab navigator

--- Navigation.js
+++ Navigation.js
@@ -1,6 +1,9 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 
+//icons
+import { FontAwesome } from '@expo/vector-icons';
+
 //screens
 import About from './screens/About';
 import Age from './screens/Age';
@@ -17,21 +20,45 @@ function MyTabs() {
             <Tab.Screen
                 name="Home"
                 component={HomeScreen}
+                options={{
+                    tabBarLabel: 'Pagina principal',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="home" size={24} color={color} />
+                    ),
+                }}
             />
 
             <Tab.Screen
                 name="Gender"
                 component={Gender}
+                options={{
+                    tabBarLabel: 'Genero',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="transgender" size={24} color={color} />
+                    ),
+                }}
             />
 
             <Tab.Screen
                 name="Age"
                 component={Age}
+                options={{
+                    tabBarLabel: 'Edad',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="users" size={24} color={color} />
+                    ),
+                }}
             />
 
             <Tab.Screen
                 name="University"
                 component={University}
+                options={{
+                    tabBarLabel: 'Universidad',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="university" size={24} color={color} />
+                    ),
+                }}
             />
 
             <Tab.Screen
@@ -42,6 +69,12 @@ function MyTabs() {
             <Tab.Screen
                 name="About"
                 component={About}
+                options={{
+                    tabBarLabel: 'Sobre mi',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="info-circle" size={24} color={color} />
+                    ),
+                }}
             />
         </Tab.Navigator>
     )
